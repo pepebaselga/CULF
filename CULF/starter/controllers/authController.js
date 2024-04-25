947ea5aebf0d817305cf464250f37d793e222bd5
Pepe Baselga-Garriga <pepebaselgagarriga@gmail.com>
2024-04-25 18:20:23 -0400

    worked on password reset functionality

--- CULF/starter/controllers/authController.js
+++ CULF/starter/controllers/authController.js
@@ -3,6 +3,7 @@ const catchAsync = require('../utils/catchAsync');
 const jwt = require('jsonwebtoken');
 const AppError = require('../utils/appError');
 const sendEmail = require('../utils/email');
+const crypto = require('crypto');
 
 const { promisify } = require('util');
 const { reset } = require('nodemon');
@@ -145,4 +146,37 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
     );
   }
 });
-exports.resetPassword = (req, res, next) => {};
+exports.resetPassword = catchAsync(async (req, res, next) => {
+  //1) Get user basked on token
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex');
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() }
+  });
+
+  //2) If token has not expired, and there is a user, set the new password
+  if (!user) {
+    return next(new AppError('Token is invalid or has expired'), 400);
+  }
+
+  //3) Update changePasswordAt property for the user
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  // if (!(user.passwordConfirm === user.password)) {
+  //   return next(new AppError('Passwords do not match, please try again!', 400));
+  // }
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+  user.changedPasswordAt = Date.now(); //TODO: I don't think that this is updating correctly
+  console.log(user.changedPasswordAt);
+  await user.save();
+  //4) Log the user in, send JWT
+  const token = signToken(user._id);
+  res.status(200).json({
+    status: 'success',
+    token: token
+  });
+});
